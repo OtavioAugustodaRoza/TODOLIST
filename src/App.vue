d83ddd8a97db823dd5cd15b2dbcb69e06130000f
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2026-03-13 15:13:07 -0300

    FEAT: finish TodoList

--- src/App.vue
+++ src/App.vue
@@ -4,44 +4,105 @@ import { ref } from 'vue'
 let tarefas = ref([
   { id: 1, desc: 'fazer o breakdance', status: 'pendente' },
   { id: 2, desc: 'ler livros', status: 'pendente' },
-  { id: 3, desc: 'estudar o neymar', status: 'concluida' }
-]);
+  { id: 3, desc: 'estudar o neymar', status: 'concluida' },
+])
 const textoDoIntput = ref('')
-let maiorID = Math.max(...tarefas.value.map(item => item.id));
-const add = (() => {
-  const tarefa = {id: maiorID+1, desc:textoDoIntput, status:'pendente'}
-  i
-  tarefas.value.push(tarefa);
-})
 
-const remove = ((id) => {
-  const i = tarefas.value.findIndex(x => x.id == id);
-  if(i >= 0){
-    tarefas.value.splice(i,1)
+const add = () => {
+  let maiorID = Math.max(...tarefas.value.map((item) => item.id),0)
+  const tarefa = { id: maiorID + 1, desc: textoDoIntput.value, status: 'pendente' }
+  if (textoDoIntput.value == '') {
+    alert('Escreva algo')
+  } else {
+    tarefas.value.push(tarefa)
   }
-} )
+  textoDoIntput.value= '';
+}
 
-const concluir = ((id) => {
-    const i = tarefas.value.findIndex(x => x.id === id)
-    if(tarefas.value[i].status == 'pendente'){
-      tarefas.value[i].status = "concluida"
-    }else{
-      tarefas.value[i].status = "pendente"
-    }
-  })
+const remove = (id) => {
+  const i = tarefas.value.findIndex((x) => x.id == id)
+  if (i >= 0) {
+    tarefas.value.splice(i, 1)
+  }
+}
 
+const concluir = (id) => {
+  const i = tarefas.value.findIndex((x) => x.id === id)
+  if(i === -1) return;
+  if (tarefas.value[i].status == 'pendente') {
+    tarefas.value[i].status = 'concluida'
+  } else {
+    tarefas.value[i].status = 'pendente'
+  }
+  console.log(tarefas.value[i].status) //so pra testar por enquanto
+}
 </script>
 <template>
-  <div class="cotainer">
-  <input type="text" v-model="textoDoIntput" @keyup.enter="add"> <button @click="add">adiconar</button>
-  <ul>
-  <li v-for=" item in tarefas" :key="item.id">
-  <div @click="concluir" >{{ item.desc }}</div>  <button @click="remove(item.id)">remover</button>
-
-
-  </li>
-
-  </ul>
+  <div class="container">
+    <input type="text" v-model="textoDoIntput" @keyup.enter="add" />
+    <button @click="add">Adicionar</button>
+    <ul>
+      <li v-for="item in tarefas" :key="item.id">
+        <div class="tarefa" :class="item.status === 'concluida' ? 'concluido' : '' " @click="concluir(item.id)">{{ item.desc }}</div>
+        <button @click="remove(item.id)">remover</button>
+      </li>
+    </ul>
   </div>
 </template>
-<style scoped></style>
+<style scoped>
+.container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 40px;
+  gap: 20px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+ul{
+  display: flex ;
+  flex-direction: column;
+  gap: 20px;
+  list-style: none;
+  padding: 0;
+  margin-top: 30px;
+  width: 300px;
+}
+li{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #f4f4f4;
+  padding: 12px;
+  border-radius: 8px;
+}
+.tarefa{
+  cursor: pointer;
+}
+.concluido{
+  text-decoration: line-through;
+  color: gray;
+}
+button{
+  padding: 10px 16px;
+  border: none;
+  border-radius: 8px;
+  background-color: #42b883;
+  color: white;
+  cursor: pointer;
+  transition: 0.3s;
+}
+input{
+  padding: 10px;
+  border-radius: 8px;
+  border: 1px solid #ccc;
+  width: 220px;
+  margin-right: 10px;
+  outline: none;
+}
+input:focus{
+  outline: #369870 solid;
+}
+button:hover{
+  background-color: #369870;
+}
+</style>
